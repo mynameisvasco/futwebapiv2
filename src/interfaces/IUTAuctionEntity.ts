1f3548f1cb5963753eaca4594909211f2a4c48bc
Vasco Sousa <myemailvasco@gmail.com>
2020-12-18 21:57:10 +0000

    :refactor: replaced interface methods by props

--- src/interfaces/IUTAuctionEntity.ts
+++ src/interfaces/IUTAuctionEntity.ts
@@ -1,24 +1,4 @@
 export interface IUTAuctionEntity {
-  canBid(): boolean;
-  canBuy(): boolean;
-  canWatch(): boolean;
-  getAge(): number;
-  getAgeInSeconds(): number;
-  getSecondsRemaining(): number;
-  isActiveTrade(): boolean;
-  isBid(): boolean;
-  isBought(): boolean;
-  isClosedTrade(): boolean;
-  isExpired(): boolean;
-  isHighestBid(): boolean;
-  isInactive(): boolean;
-  isOutbid(): boolean;
-  isPendingExpiry(): boolean;
-  isSelling(): boolean;
-  isSold(): boolean;
-  isStale(): boolean;
-  isValid(): boolean;
-  isWon(): boolean;
   buyNowPrice: number;
   currentBid: number;
   expires: number;
